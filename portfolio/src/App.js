f41efdea2d16290a9ee6063976f33b585eceb07a
HyonNeo <128829684+HyonNeo@users.noreply.github.com>
2023-09-17 13:31:31 -0400

    macked the app.js base to start

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [scroll, setScroll] = React.useState(false);
+
+
+  React.useEffect(() => {
+    window.addEventListener('scroll', () => {
+      switch (true) {
+        case window.innerWidth <= 1280 && window.innerWidth >= 736:
+          setScroll(window.scrollY > 337);
+          break;
+        case window.innerWidth <= 736 && window.innerWidth >= 564:
+          setScroll(window.scrollY > 311);
+          break;
+        case window.innerWidth <= 564 && window.innerWidth >= 480:
+          setScroll(window.scrollY > 377);
+          break;
+        default:
+          setScroll(window.scrollY > 477);
+          break;
+      }
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div id="wrapper">
+      <header id="header">
+        <h1>Facundo Garay</h1>
+        <p>Junior Full Stack Web Developer</p>
       </header>
+
+      <nav id="nav" className={scroll ? 'alt' : null}>
+        <ul>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#skills">Skills</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contacts</a>
+          </li>
+        </ul>
+      </nav>
+
+      <div id="main">
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+      </div>
+
+      <Footer />
     </div>
   );
 }
-
-export default App;
